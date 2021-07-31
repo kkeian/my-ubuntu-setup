34f34689994c57ff1cfcf8c3337ce8f0170904b6
kkeian <kaserman22@gmail.com>
2021-07-30 22:37:52 -0400

    Fix upgrade script to delete old go version in a safer way

--- .bash/go/upgrade-go.sh
+++ .bash/go/upgrade-go.sh
@@ -4,24 +4,20 @@ go env GOROOT 2> /dev/null
 STATUS=$?
 if [ $STATUS -eq 0 ]; then
 	GOROOT=$(go env GOROOT)
-	OLDGO="${GOROOT}-old"
 elif [ $STATUS -eq 1 ] || [ $STATUS -eq 2 ]; then
 	GOROOT=/usr/local
-	OLDGO=""
 fi
 
-
-# Change name of old go base dir
-if [ ! $GOROOT == "" ]; then
-	sudo mv $GOROOT $OLDGO
+TEMP=/tmp
+# Move old go version to /tmp to be erased on next boot
+if [ ! $GOROOT == "/usr/local" ]; then
+	sudo mv $GOROOT $TEMP
+	OLDGO="${TEMP}${GOROOT}" 
+	# Rename folder to something recognizable
+	sudo mv $OLDGO "${OLDGO}-old"
 fi
 
 # Unpack downloaded new go version 
 NEWGO=$(ls $HOME/Downloads | grep 'go*' | sort | tail -n1)
 DOWNLOADS=$HOME/Downloads
 cd $DOWNLOADS && sudo tar -C /usr/local -xzf $NEWGO
-
-# Remove old version of go from system
-if [ ! $OLDGO == "" ]; then
-	rm -r $OLDGO
-fi
