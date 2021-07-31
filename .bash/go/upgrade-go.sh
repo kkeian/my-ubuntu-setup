55acb741d8f9e555d214bae10d8ba250d413532e
kkeian <kaserman22@gmail.com>
2021-07-30 21:53:38 -0400

    Provide error handling for finding old Go verison

--- .bash/go/upgrade-go.sh
+++ .bash/go/upgrade-go.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-GOROOT=$(go env GOROOT)
+go env GOROOT
+STATUS=$?
+if [ $STATUS -eq 0 ]; then
+	GOROOT=$(go env GOROOT)
+elif [ $STATUS -eq 1 ] || [ $STATUS -eq 2 ]; then
+	GOROOT=/usr/local
+fi
+
 OLDGO="${GOROOT}-old"
+
 # Change name of old go base dir
-sudo mv $GOROOT $OLDGO
+if [ $GOROOT != "" ]; then
+	sudo mv $GOROOT $OLDGO
+fi
 
 # Unpack downloaded new go version 
 NEWGO=$(ls $HOME/Downloads | grep 'go*' | sort | tail -n1)
-NEWGOLOC=$(find $HOME/Downloads -name 'go*' -type f | sort | tail -n1)
-cd $NEWGOLOC && sudo tar -C /usr/local -xzf $NEWGO
+DOWNLOADS=$HOME/Downloads
+cd $DOWNLOADS && sudo tar -C /usr/local -xzf $NEWGO
 
 # Remove old version of go from system
 sudo rm -rf $OLDGO
