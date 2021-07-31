4b6ce2878ec8dc74ac66bef8077727aacdd45f0b
kkeian <kaserman22@gmail.com>
2021-07-30 21:30:28 -0400

    Update upgrade-go.sh to include correct logic to unpack new go version into proper location

--- .bash/go/upgrade-go.sh
+++ .bash/go/upgrade-go.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-# ensure this is run as root
-if [[ $EUID -ne 0 ]]; then
-	echo "Permission denied"
-	exit 1
-fi
 
 GOROOT=$(go env GOROOT)
-OLDGO="$GOROOT-old"
+OLDGO="${GOROOT}-old"
 # Change name of old go base dir
 sudo mv $GOROOT $OLDGO
 
 # Unpack downloaded new go version 
 NEWGO=$(ls $HOME/Downloads | grep 'go*' | sort | tail -n1)
-sudo tar -C /usr/local -xzf $NEWGO
+NEWGOLOC=$(find $HOME/Downloads -name 'go*' -type f | sort | tail -n1)
+cd $NEWGOLOC && sudo tar -C /usr/local -xzf $NEWGO
 
 # Remove old version of go from system
 sudo rm -rf $OLDGO
